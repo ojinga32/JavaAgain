e28d08fa744f5b1ec0d0e20b0a7da0f123e32d52
ojinga32 <ojinga32@gmail.com>
2023-11-22 22:23:55 +0900

    firstCommit

---
+++ FirstJavaProjectWithin28Minutes/src/com/in28minutes/firstjavaproject/Car.java
@@ -0,0 +1,17 @@
+package com.in28minutes.firstjavaproject;
+
+public class Car {
+
+	// 프로그래머로서 일을 시작한지 2개월이지만 기초가 많이 부족한것 같다.
+	
+	private int speed;
+
+	public int setSpeed(int speed) {
+		return this.speed = speed;
+	}
+	
+	public int getSpeed() {
+		return this.speed = speed;
+	}
+	
+}
---
+++ FirstJavaProjectWithin28Minutes/src/com/in28minutes/firstjavaproject/Result.java
@@ -0,0 +1,9 @@
+package com.in28minutes.firstjavaproject;
+
+public class Result {
+	public static void main(String[] args) {
+		Car car = new Car();
+		car.setSpeed(100);
+//		System.out.println(car.getSpeed());
+	}
+}
