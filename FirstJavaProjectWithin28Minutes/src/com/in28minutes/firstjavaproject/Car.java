27b3673ce0220d09e18975e3336c97a6fa89191d
ojinga32 <ojinga32@gmail.com>
2023-11-23 22:20:53 +0900

    this를 사용하여 setter, getter의 원리 파악. 생성자란

--- FirstJavaProjectWithin28Minutes/src/com/in28minutes/firstjavaproject/Car.java
+++ FirstJavaProjectWithin28Minutes/src/com/in28minutes/firstjavaproject/Car.java
@@ -3,9 +3,19 @@ package com.in28minutes.firstjavaproject;
 public class Car {
 
 	// 프로그래머로서 일을 시작한지 2개월이지만 기초가 많이 부족한것 같다.
-	
 	private int speed;
-
+	private int power;
+	
+	public Car(int speed) {
+		this.speed = speed;
+	}
+	
+	public Car(int speed ,int power) {
+		this.speed = speed;
+		this.power = power;
+		
+	}
+	
 	public int setSpeed(int speed) {
 		return this.speed = speed;
 	}
@@ -13,5 +23,14 @@ public class Car {
 	public int getSpeed() {
 		return this.speed = speed;
 	}
+
+	@Override
+	public String toString() {
+		return "Car [speed=" + speed + ", power=" + power + "]";
+	}
+	
+	
+	
+	
 	
 }
--- FirstJavaProjectWithin28Minutes/src/com/in28minutes/firstjavaproject/Result.java
+++ FirstJavaProjectWithin28Minutes/src/com/in28minutes/firstjavaproject/Result.java
@@ -2,8 +2,8 @@ package com.in28minutes.firstjavaproject;
 
 public class Result {
 	public static void main(String[] args) {
-		Car car = new Car();
-		car.setSpeed(100);
-//		System.out.println(car.getSpeed());
+		Car car = new Car(100,400);
+//		car.setSpeed(100);
+		System.out.println(car.toString());
 	}
 }
