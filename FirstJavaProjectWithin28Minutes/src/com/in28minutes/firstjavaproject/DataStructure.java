4fe53ef71e9a9940d8d20c4125c38f3783141cba
ojinga32 <ojinga32@gmail.com>
2023-12-06 22:19:56 +0900

    basic of datastrcture

---
+++ FirstJavaProjectWithin28Minutes/src/com/in28minutes/firstjavaproject/DataStructure.java
@@ -0,0 +1,16 @@
+package com.in28minutes.firstjavaproject;
+
+public class DataStructure {
+	public static void main(String[] args) {
+		/*
+			List : 중복 값 저장 가능
+		 	Set : 중복 값 저장 불가능
+		 	Queue : 순서가 정해지고 모든 요소가 한번만 처리됨
+		 	Map : (열쇠,값) 열쇠 중복 불가능, 값 중복 가능
+		 	
+		 	Hash : 순서x, 정렬x
+		 	Linked : 순서o, 정렬x
+		 	Tree : 순서x, 정렬o			
+		*/
+	}
+}
