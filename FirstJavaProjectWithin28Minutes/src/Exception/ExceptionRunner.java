cf558659faf97fca64199d6c7ec9ec0b76dcaaed
ojinga32 <ojinga32@gmail.com>
2023-12-12 22:17:14 +0900

    예외 처리 1

---
+++ FirstJavaProjectWithin28Minutes/src/Exception/ExceptionRunner.java
@@ -0,0 +1,19 @@
+package Exception;
+
+public class ExceptionRunner {
+	public static void main(String[] args) {
+		/*
+		 	there are Error, Exception
+		 	Error is the bug i can't fix by coding
+		 	Exception is the bug i can fix by coding and prevent by excepting
+		 	
+		 	try {
+		 		write the code it might be Exception
+		 	} catch (write the type of Exception example : Exception | NullPointException ex) {
+		 		e.printStackTrace();
+		 	} finally {
+		 		it run 100%
+		 	}
+		*/
+	}
+}
